f7612a802b05d4ca176ea62df334cd15944478db
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-31 15:37:57 +0200

    created function to get random meme

--- makdum436_djs07/src/components/Meme.jsx
+++ makdum436_djs07/src/components/Meme.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import memesData from "../../../memesData";
 
 export default function Meme() {
+
+  let url;
+
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+  };
+  
   return (
     <main>
       <form className="form">
-        <input
-          type="text"
-          placeholder="Top text"
-          className="form--input"
-        ></input>
-        <input
-          type="text"
-          placeholder="Bottom text"
-          className="form--input"
-        ></input>
+        <div>
+          <label htmlFor="top-text">Top Text</label>
+          <input
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="">Bottom Text</label>
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+          ></input>
+        </div>
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
