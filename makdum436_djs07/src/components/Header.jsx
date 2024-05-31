d4d7caf1f317a2f25e7e7dfb429260fb08852862
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-31 14:43:44 +0200

    completed the header section

--- makdum436_djs07/src/components/Header.jsx
+++ makdum436_djs07/src/components/Header.jsx
@@ -2,9 +2,10 @@
 
 export default function Header() {
   return (
-    <header>
-      <h2>Header component</h2>
+    <header className="header">
+      <img src="./images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
-
