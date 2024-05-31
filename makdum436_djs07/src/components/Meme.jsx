339d2cf64f19ba0fe93f3e014c6fea3df56abb8d
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-05-31 14:54:17 +0200

    completed meme component

---
+++ makdum436_djs07/src/components/Meme.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+        ></input>
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+        ></input>
+        <button className="form--button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
